a3d80e729a8f57d987c534c1886b3d3736cf2170
dinhvanhuong <meomap2702@gmail.com>
2025-12-19 11:24:31 +0700

    cập nhập

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php 
-  // Dùng 'db' thay vì 'localhost' vì đang chạy trong Docker
-  $co = mysql_connect('db', 'root', ''); 
-  if (!$co) echo "Lỗi";
-  else echo "OK";
+<meta charset="UTF-8">
+<?php
+echo "<h1>Bài Kiểm tra - MSSV: DH52200753</h1>";
+
+$host = 'db';
+$user = 'root';
+$pass = '';
+
+try {
+    // Kết nối vào MySQL server thôi, không cần chọn database test_db
+    $conn = new PDO("mysql:host=$host", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "<h3>Kết nối Database (PDO): OK</h3>"; 
+} catch(PDOException $e) {
+    echo "<h3>Lỗi kết nối: " . $e->getMessage() . "</h3>";
+}
 ?>
