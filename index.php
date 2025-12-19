2920e10a27fd00e0e5b6948b3f39b98e56b6540b
dinhvanhuong <meomap2702@gmail.com>
2025-12-19 13:03:02 +0700

    THAY ĐỔI  CẬP NHẠP LẠI

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <meta charset="UTF-8">
 <?php
-echo "<h1>Bài Kiểm tra - MSSV: DH52200753</h1>";
+echo "<h1>1M1-CUỐIKY-DINHVANHUONG</h1>"; // Bạn có thể thêm MSSV vào đây nếu cần
 
 $host = 'db';
 $user = 'root';
 $pass = '';
 
 try {
-    // Kết nối vào MySQL server thôi, không cần chọn database test_db
     $conn = new PDO("mysql:host=$host", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
     echo "<h3>Kết nối Database (PDO): OK</h3>"; 
 } catch(PDOException $e) {
     echo "<h3>Lỗi kết nối: " . $e->getMessage() . "</h3>";
